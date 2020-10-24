3e71ccf16e088d63653d9b53a83170cd8b6b6f4d
MananPyJava <ardumanan@gmail.com>
2020-10-24 14:05:26 +0000

    added replit db interface

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Flask
-app = Flask('app')
+from replit import db
 
+db.get('views', {})
+app = Flask('app')
+a=0
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+	global a
+	a+=1
+	db["views"] = a
+	return 'Hello, World!'+str(db['views'])
+
 
 app.run(host='0.0.0.0', port=8080)
